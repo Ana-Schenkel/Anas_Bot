8b536926e6448ed138d1dd79b5d4a0006677ea87
Ana Schenkel <anaschnkl@gmail.com>
2023-10-14 00:12:38 -0300

    Primeiro teste do bot, teste padrão ping pong

--- anas_bot/anas_bot/anas_bot.py
+++ anas_bot/anas_bot/anas_bot.py
@@ -1,1 +1,32 @@
-"""Main module."""
+# """Main module."""
+
+import os
+
+import discord
+
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print("Logado como", self.user)
+
+    async def on_message(self, message):
+        # don't respond to ourselves
+        if message.author == self.user:
+            return
+
+        if message.content == "ping":
+            await message.channel.send("pong")
+
+
+intents = discord.Intents.default()
+intents.message_content = True
+client = MyClient(intents=intents)
+
+user = os.getcwd()
+token = open(
+    user + "\\anas_bot\\anas_bot\\token.txt",
+    encoding="utf-8",
+    mode="r",
+)
+token = token.read()
+client.run(token)
