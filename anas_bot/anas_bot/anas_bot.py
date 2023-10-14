4aedd6b24f68dabbab719587794a97bee704e7e1
Ana Schenkel <anaschnkl@gmail.com>
2023-10-14 17:28:25 -0300

    Bot inicial com funções de comando adequadas

--- anas_bot/anas_bot/anas_bot.py
+++ anas_bot/anas_bot/anas_bot.py
@@ -1,32 +1,77 @@
 # """Main module."""
 
 import os
-
 import discord
+from discord.ext import commands
 
-
-class MyClient(discord.Client):
-    async def on_ready(self):
-        print("Logado como", self.user)
-
-    async def on_message(self, message):
-        # don't respond to ourselves
-        if message.author == self.user:
-            return
-
-        if message.content == "ping":
-            await message.channel.send("pong")
-
-
-intents = discord.Intents.default()
-intents.message_content = True
-client = MyClient(intents=intents)
-
+bot = commands.Bot(command_prefix="$", intents=discord.Intents.all())
 user = os.getcwd()
+
+
+@bot.event
+async def on_ready():
+    print("Logado")
+
+
+@bot.command()
+async def ajuda(ctx):
+    ajuda = open(
+        user + "\\anas_bot\\anas_bot\\comandos\\help.txt",
+        encoding="utf-8",
+        mode="r",
+    )
+    ajuda = ajuda.read()
+    await ctx.send(ajuda)
+
+
+@bot.command()
+async def info(ctx):
+    info = open(
+        user + "\\anas_bot\\anas_bot\\comandos\\info.txt",
+        encoding="utf-8",
+        mode="r",
+    )
+    info = info.read()
+    await ctx.send(info)
+
+
+@bot.command()
+async def forca(ctx):
+    forca = open(
+        user + "\\anas_bot\\anas_bot\\comandos\\instru_forca.txt",
+        encoding="utf-8",
+        mode="r",
+    )
+    forca = forca.read()
+    await ctx.send(forca)
+
+
+@bot.command()
+async def velha(ctx):
+    velha = open(
+        user + "\\anas_bot\\anas_bot\\comandos\\instru_velha.txt",
+        encoding="utf-8",
+        mode="r",
+    )
+    velha = velha.read()
+    await ctx.send(velha)
+
+
+@bot.command()
+async def hanoi(ctx):
+    hanoi = open(
+        user + "\\anas_bot\\anas_bot\\comandos\\instru_hanoi.txt",
+        encoding="utf-8",
+        mode="r",
+    )
+    hanoi = hanoi.read()
+    await ctx.send(hanoi)
+
+
 token = open(
     user + "\\anas_bot\\anas_bot\\token.txt",
     encoding="utf-8",
     mode="r",
 )
 token = token.read()
-client.run(token)
+bot.run(token)
