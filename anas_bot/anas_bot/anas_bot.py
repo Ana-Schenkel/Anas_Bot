898f2b7ab9e203b6aabc45b47455e0bd2334c7d3
Ana Schenkel <anaschnkl@gmail.com>
2023-10-18 23:01:45 -0300

    Esboço menu jogo da velha e forca pronto

--- anas_bot/anas_bot/anas_bot.py
+++ anas_bot/anas_bot/anas_bot.py
@@ -13,90 +13,6 @@ async def on_ready():
     print("Logado")
 
 
-@bot.event
-async def on_message(message):
-    print(message.content)
-    print(message.channel)
-
-    def check(m):
-        autor = m.channel == channel and m.author == jogador
-        resposta = m.content == "1" or m.content == "2"
-        return autor and resposta
-
-    def check_privado(m):
-        return m.author == jogador
-
-    def check_member(m):
-        mention = m.mentions
-        if len(mention) > 1 or mention == []:
-            return False
-        else:
-            return True
-
-    if message.content.startswith("$forca"):
-        channel = message.channel
-        jogador = message.author
-
-        forca = open(
-            user + "\\anas_bot\\anas_bot\\comandos\\instru_forca.txt",
-            encoding="utf-8",
-            mode="r",
-        )
-        forca = forca.read()
-        await channel.send(forca)
-
-        await channel.send(
-            "Escolha uma opção: \n 1 - Sortear uma palavra \n 2 - Desafiar um amigo"
-        )
-
-        msg = await bot.wait_for("message", check=check)
-
-        if msg.content == "1":
-            await channel.send(f"Hello {msg.author}!")
-            # palavra = forca.sortea_palavra()
-        elif msg.content == "2":
-            await jogador.send("Digite a palavra que você deseja que adivinhem!")
-            palavra = await bot.wait_for("message", check=check_privado)
-            palavra = palavra.content
-            print(palavra)
-            await channel.send(f"Oi {msg.author}!")
-            await channel.send("Digite quem você deseja desafiar")
-            jogador1 = await bot.wait_for("message", check=check_member)
-            jogador1 = jogador1.mentions
-            jogador1 = jogador1[0]
-            await channel.send(f"{jogador1} você foi desafiado para o jogo da forca!")
-            # palavra = o que ele disser no privado
-
-    if message.content.startswith("$velha"):
-        channel = message.channel
-        jogador = message.author
-
-        velha = open(
-            user + "\\anas_bot\\anas_bot\\comandos\\instru_velha.txt",
-            encoding="utf-8",
-            mode="r",
-        )
-        velha = velha.read()
-        await channel.send(velha)
-
-        await channel.send(
-            "Escolha uma opção: \n 1 - Jogar com o bot \n 2 - Desafiar um amigo"
-        )
-
-        msg = await bot.wait_for("message", check=check)
-
-        if msg.content == "1":
-            await channel.send(f"Hello {msg.author}!")
-            # começar jogo da velha
-        elif msg.content == "2":
-            await channel.send("Marque o amigo com quem vai jogar")
-            jogador1 = await bot.wait_for("message", check=check_member)
-            jogador1 = jogador1.mentions
-            jogador1 = jogador1[0]
-            await channel.send(f"{jogador1} você foi desafiado para o jogo da velha!")
-            # palavra = o que ele disser no privado
-
-
 @bot.command()
 async def help(ctx):
     ajuda = open(
@@ -119,49 +35,125 @@ async def info(ctx):
     await ctx.send(info)
 
 
-# @bot.command()
-# async def forca(ctx):
-#     forca = open(
-#         user + "\\anas_bot\\anas_bot\\comandos\\instru_forca.txt",
-#         encoding="utf-8",
-#         mode="r",
-#     )
-#     forca = forca.read()
-#     await ctx.send(forca)
+@bot.event
+async def on_message(message):
+    await bot.process_commands(message)  # faz os comandos processarem
+
+    # print(message.content)
+    # print(message.channel)
+    # print(bot.user)
+    channel = message.channel
+    jogador = message.author
+
+    def check_menu(m):
+        autor = m.channel == channel and m.author == jogador
+        resposta = m.content == "1" or m.content == "2"
+        return autor and resposta
+
+    def check_mention(m):
+        autor = m.channel == channel and m.author == jogador
+        mention = m.mentions
+        if len(mention) > 1 or mention == []:
+            return False
+        else:
+            return True and autor
+
+    # ************************************************* Jogo Forca *************************************************
+
+    if message.content.startswith("$forca"):
+        forca = open(
+            user + "\\anas_bot\\anas_bot\\comandos\\instru_forca.txt",
+            encoding="utf-8",
+            mode="r",
+        )
+        forca = forca.read()
+        await channel.send(forca)
+
+        await channel.send(
+            "Escolha uma opção: \n 1 - Sortear uma palavra \n 2 - Desafiar um amigo"
+        )
+
+        msg = await bot.wait_for("message", check=check_menu)
+
+        if msg.content == "1":
+            await channel.send(f"Hello {msg.author}, uma palavra vai ser sorteada!")
+            # palavra = forca.sortea_palavra()
+        elif msg.content == "2":
+            await jogador.send("Digite a palavra que você deseja que adivinhem!")
+            palavra = await bot.wait_for(
+                "message",
+                check=lambda x: x.channel == jogador.dm_channel and x.author == jogador,
+            )
+            palavra = palavra.content
+            await channel.send("Digite quem você deseja desafiar")
+            jogador1 = await bot.wait_for("message", check=check_mention)
+            jogador1 = jogador1.mentions
+            jogador1 = jogador1[0]
+            await channel.send(f"{jogador1} você foi desafiado para o jogo da forca!")
+            # palavra = o que ele disser no privado
+
+    # ************************************************* Jogo da Velha *************************************************
+
+    if message.content.startswith("$velha"):
+        velha = open(
+            user + "\\anas_bot\\anas_bot\\comandos\\instru_velha.txt",
+            encoding="utf-8",
+            mode="r",
+        )
+        velha = velha.read()
+        await channel.send(velha)
+
+        await channel.send(
+            "Escolha uma opção: \n 1 - Jogar com o bot \n 2 - Desafiar um amigo"
+        )
+        msg = await bot.wait_for("message", check=check_menu)
+
+        if msg.content == "1":
+            await channel.send(f"Hello {msg.author}, você vai jogar contra o bot!")
+            jogador1 = bot.user
+            # adicionar jogador bot
+        elif msg.content == "2":
+            await channel.send("Marque o amigo com quem vai jogar")
+            jogador1 = await bot.wait_for("message", check=check_mention)
+            jogador1 = jogador1.mentions
+            jogador1 = jogador1[0]
+            await channel.send(f"{jogador1} você foi desafiado para o jogo da velha!")
+            # adicionar outro jogador
+
+        await channel.send(
+            "Escolha uma opção: \n 1 - Sortear ordem de jogada \n 2 - Escolher o primeiro jogador"
+        )
+        msg = await bot.wait_for("message", check=check_menu)
+
+        if msg.content == "1":
+            await channel.send(f"Hello {msg.author}, vamos começar!")
+            # sortear começar jogo da velha
+        elif msg.content == "2":
+            await channel.send("Marque quem irá jogar primeiro")
+            primeiro_jogador = await bot.wait_for("message", check=check_mention)
+            primeiro_jogador = primeiro_jogador.mentions
+            primeiro_jogador = primeiro_jogador[0]
+            if primeiro_jogador == jogador:
+                await channel.send(f"Hello {jogador}, você é o primeiro!")
+                # ordenar jogador como primeiro
+            elif primeiro_jogador == jogador1:
+                await channel.send(f"Hello {jogador1}, você é o primeiro!")
+                # ordenar jogador1 como primeiro
+            # começar jogo da velha
+
+    # ************************************************* Torre de Hanoi *************************************************
+
+    if message.content.startswith("$hanoi"):
+        velha = open(
+            user + "\\anas_bot\\anas_bot\\comandos\\instru_hanoi.txt",
+            encoding="utf-8",
+            mode="r",
+        )
+        velha = velha.read()
+        await channel.send(velha)
 
 
-# @bot.command()
-# async def velha(ctx, posicao=None):
-#     member = ctx.author
-#     print(posicao)
-#     print(member)
-#     if posicao == None:
-#         velha = open(
-#             user + "\\anas_bot\\anas_bot\\comandos\\instru_velha.txt",
-#             encoding="utf-8",
-#             mode="r",
-#         )
-#         velha = velha.read()
-#         await ctx.send(velha)
-
-#     def check(m):
-#         return m.content == "hello" and m.channel == ctx.channel
-
-#     msg = await bot.wait_for("on_message")
-#     await ctx.send(f"Hello {msg.author}!")
-
-
-# @bot.command()
-# async def hanoi(ctx):
-#     hanoi = open(
-#         user + "\\anas_bot\\anas_bot\\comandos\\instru_hanoi.txt",
-#         encoding="utf-8",
-#         mode="r",
-#     )
-#     hanoi = hanoi.read()
-#     await ctx.send(hanoi)
-
-
+# roda o bot definido no token
 token = open(
     user + "\\anas_bot\\anas_bot\\token.txt",
     encoding="utf-8",
